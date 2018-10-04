a234f504693a30be1a3d06045d22661497365b86
Dupeljan Linux <dupeljan@gmail.com>
2018-10-04 10:00:24 +0300

    Init commit

---
+++ fontan.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import wave
+import numpy as np
+import math
+
+types = {
+    1: np.int8,
+    2: np.int16,
+    3: np.dtype([('first_byte_pair', '<i2'), ('second_pair', '<u1')]),
+    4: np.int32
+}
+
+
+def format_db(x, pos=None):
+    if pos == 0:
+        return ""
+    global peak
+    if x == 0:
+        return "-inf"
+
+    db = 20 * math.log10(abs(x) / float(peak))
+    return int(db)
+
+wav = wave.open("music.wav", mode="r")
+(nchannels, sampwidth, framerate, nframes, comptype, compname) = wav.getparams()
+
+duration = nframes / framerate
+w, h = 800, 300
+k = nframes/w/32
+peak = 256 ** sampwidth / 2
+
+content = wav.readframes(nframes)
+samples = np.fromstring(content, dtype=types[sampwidth])
+
+
+for n in range(nchannels):
+    channel = samples[n::nchannels]
+
+    channel = channel[0::k]
+    if nchannels == 1:
+        channel = channel - peak
+    for i in range(len(channel)):
+    	print(channel[i])    
+   
